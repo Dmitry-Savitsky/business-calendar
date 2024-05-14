1ccd652fffbf8418a596a14d433637b414678d14
Dmitry-Savitsky <dmsw0rk333@gmail.com>
2024-05-15 00:03:44 +0300

    init: back, company start + docker

---
+++ WebApplication1/WebApplication1/Controllers/ExecutorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/executors")]
+    [ApiController]
+    public class ExecutorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetExecutors()
+        {
+            var executors = new[]
+            {
+                new {Name = "Dmitry"},
+                new {Name = "Oleg"}
+            };
+            return Ok(executors);
+        }
+    }
+}
