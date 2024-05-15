63ca1bc7f3c8e412eb8594504634c55e16cc2005
Dmitry-Savitsky <dmsw0rk333@gmail.com>
2024-05-16 00:09:24 +0300

    feat: adding nginx proxy + api accessibility check

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccd652fffbf8418a596a14d433637b414678d14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8571a41793d6603eabd780fccc14397c476f8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApplication1/company/src/App.js
+++ WebApplication1/company/src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      executors: []
+    };
+
+    // Привязываем метод GetExecutors к контексту класса
+    this.GetExecutors = this.GetExecutors.bind(this);
+  }
+
+  // Объявляем GetExecutors как метод класса
+  GetExecutors = async () => {
+    var response = await fetch('api/executors', {
+      method: 'get'
+    });
+
+    var responsejson = await response.json();
+    this.setState({
+      executors: responsejson
+    });
+  };
+
+  render() {
+    const executors = this.state.executors.map((item, index) => (
+      <li key={index}>{item.name}</li>
+    ));
+    return (
+      <div className='App'>
+        <button onClick={this.GetExecutors}>Загрузить список пользователей</button>
+        <ul>{executors}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
