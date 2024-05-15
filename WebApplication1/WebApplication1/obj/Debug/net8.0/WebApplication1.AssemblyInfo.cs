aa8571a41793d6603eabd780fccc14397c476f8e
Dmitry-Savitsky <dmsw0rk333@gmail.com>
2024-05-15 22:24:52 +0300

    feat: cors policy update

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b146151a18b3636c25ef096a3c84bf14d43a48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccd652fffbf8418a596a14d433637b414678d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
